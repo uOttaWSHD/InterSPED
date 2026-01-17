69abd90c6079940ae1f0ee8fd1a777466c2007b8
Dan <danielshan824@gmail.com>
2026-01-17 01:03:33 -0500

    Creating my own branch
    Waaberi's assertions are crashing my code (I don't have his API key)

--- JobScraper/main.py
+++ JobScraper/main.py
@@ -294,10 +294,11 @@ async def get_examples() -> dict[str, Any]:
 
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=settings.debug
-    )
+    pass
+    # import uvicorn
+    # uvicorn.run(
+    #     "main:app",
+    #     host="0.0.0.0",
+    #     port=8000,
+    #     reload=settings.debug
+    # )
